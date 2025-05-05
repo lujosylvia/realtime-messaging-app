317228aedb1e748245ccba1618f512487a2c3f7f
Sylvia Lujo <sylvialujo@Sylvias-MBP.lan>
2025-05-05 18:06:55 -0400

    fallback redirect uri

--- src/app/components/providers/index.tsx
+++ src/app/components/providers/index.tsx
@@ -4,7 +4,7 @@ import cognitoAuthConfig from '@/config/aws-cognito';
 import config from '@/config/aws-eventapi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Amplify } from 'aws-amplify';
-import react from 'react';
+import react, { useEffect, useState } from 'react';
 import { AuthProvider } from 'react-oidc-context';
 
 const queryClient = new QueryClient();
@@ -12,8 +12,19 @@ const queryClient = new QueryClient();
 Amplify.configure(config);
 
 export default function Providers({ children }: { children: react.ReactNode }) {
+  const [cognitoConfig, setCognitoConfig] = useState(cognitoAuthConfig);
+  
+  useEffect(() => {
+    const origin = window?.location?.origin || 'http://localhost:3000';
+    const config = {
+      ...cognitoAuthConfig,
+      redirect_uri: `${origin}/conversation`,
+    };
+    setCognitoConfig(config);
+  }, []);
+
   return (
-    <AuthProvider {...cognitoAuthConfig}>
+    <AuthProvider {...cognitoConfig}>
       <QueryClientProvider client={queryClient}>
         {children}
       </QueryClientProvider>
